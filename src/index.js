2696c9b70caa31147c272fdc6fcc2f33cd464a8d
yashwant singh <yashwantsingh1308@gmail.com>
2024-08-01 11:18:56 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,47 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is running at port :${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MONGO db connect nahi hua !!",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
